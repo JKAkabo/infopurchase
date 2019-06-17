ed86a286aa9673e60259ff910f6cd40d1c22d358
John Kennedy <developer.akabojohnkennedy@gmail.com>
2019-06-17 16:01:29 +0000

    Create inital laravel project and migrations

---
+++ database/factories/ItemInventoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\ItemInventory;
+use Faker\Generator as Faker;
+
+$factory->define(ItemInventory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/SupplierItemFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\SupplierItem;
+use Faker\Generator as Faker;
+
+$factory->define(SupplierItem::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
